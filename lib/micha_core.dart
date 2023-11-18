9b9d33e88b438d6b7acfddc9b86c9f992a80b589
Micha Sengotta <micha.sengotta@gmail.com>
2023-11-18 10:58:35 +0100

    Move private sources to lib/src to follow convention

--- lib/micha_core.dart
+++ lib/micha_core.dart
@@ -1,7 +1,7 @@
 library micha_core;
 
-export 'extensions/separated.dart';
-export 'extensions/times.dart';
-export 'layout/gap.dart';
-export 'text/themed_text.dart';
-export 'extensions/by_name_or_null.dart';
+export 'src/extensions/by_name_or_null.dart';
+export 'src/extensions/separated.dart';
+export 'src/extensions/times.dart';
+export 'src/layout/gap.dart';
+export 'src/text/themed_text.dart';
--- test/extensions/by_name_or_null_test.dart
+++ test/extensions/by_name_or_null_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:micha_core/extensions/by_name_or_null.dart';
+import 'package:micha_core/micha_core.dart';
 
 enum TestEnum { one, two, three }
 
