8a1f7422bb24456f0b661fe58f774c59f444172c
Micha Sengotta <micha.sengotta@gmail.com>
2023-11-18 10:49:05 +0100

    Add an example project

---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:micha_core/micha_core.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Micha Core Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        extensions: const [
+          // use theme extensions to customize default values
+          GapThemeData(size: 32),
+        ],
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const itemCount = 10;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: itemCount // generate a list of a given length
+                .times<Widget>(
+                  // use themed Text without referencing BuildContext explicitly
+                  (index) => ThemedText.headlineMedium('item $index'),
+                )
+                // add a Gap in-between the Text widgets, use math operators to modify the scale.
+                .separated(const Gap() + 2),
+          ),
+        ),
+      ),
+    );
+  }
+}
